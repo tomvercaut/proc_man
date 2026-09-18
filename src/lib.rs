dd512879c8d6a4cfea41a37ee488b76cf533a478
Tom <tomvercaut@gmail.com>
2026-09-19 00:12:18 +0200

    refactor(config): update environment variable and database file extension

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@ mod error;
 pub use error::{Error, Result};
 
 pub fn default_db_path() -> Result<std::path::PathBuf> {
-    match std::env::var("PROC_MAN_DB") {
-        Ok(path) => Ok(std::path::PathBuf::from(path).join("proc_man.db")),
+    match std::env::var("PROC_MAN_DIR") {
+        Ok(path) => Ok(std::path::PathBuf::from(path).join("proc_man.sqlite")),
         Err(_) => Ok(dirs::config_local_dir()
             .ok_or(Error::ConfigLocalDirNotFound)?
             .join("proc_man")
